1d5d2c6d9b654f47fd6c6a6cda0e7452ccbf0499
nitrek <koolestnitesh@gmail.com>
2021-07-15 00:51:27 +0530

    more things

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    //vertx server setup   
     public static void main( String[] args )
     {
         Vertx vertx = Vertx.vertx();    
@@ -14,15 +21,18 @@ public class App
 
         Router router = Router.router( vertx );
 
-        router.route().handler( 
+        Route Handler1 = router.post("/hello").handler( 
             
-            RoutingContext ->{
-                routingContext.respose() = routingContext.response();
-                response.putHeader("content-type","text/plain");
-                response.end("Hello World!");
-
+            routingContext ->{
+                System.out.println( "Request received: " + routingContext.request().path() );
+                HttpServerResponse response = routingContext.response();
+                response.setChunked(true);
+                response.write("hello nitrwek");
+                response.end();
             }
-         );
-
+         ); 
+         server
+            .requestHandler(router)
+            .listen( 8090 );
     }
 }
